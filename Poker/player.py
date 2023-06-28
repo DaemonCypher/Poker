31732683f646edb5f82f51e3c7cd2500aaec197e
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-06-28 18:46:55 -0500

    Update player.py

--- Poker/player.py
+++ Poker/player.py
@@ -2,12 +2,26 @@ class Player():
     def __init__(self, name, balance, hand, folded):
         self.name = name
         self.balance = balance
-        self.hand = []
+        self.hand = hand
         self.folded = folded
+
     def getBalance(self):
         return self.balance
+
     def addBalance(self, amount):
         self.balance += amount
+
     def reduceBalance(self, amount):
         self.balance -= amount
-    
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def reset_hand(self):
+        self.hand = []
+
+    def fold(self):
+        self.folded = True
+
+    def unfold(self):
+        self.folded = False
