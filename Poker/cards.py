70ed83225b34d1afb27b2bbac3f334532f3fe3f7
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-06-28 18:51:18 -0500

    Update cards.py

--- Poker/cards.py
+++ Poker/cards.py
@@ -41,5 +41,53 @@ def displayCards(cards):
             rows[3] += '|_{}| '.format(rank.rjust(2, '_'))
 
     # Print each row on the screen:
-    for row in rows:
-        print(row)
+    # The card ranks from lowest to highest
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+def hand_value(hand):
+    """Calculate and return the value of the hand."""
+    # Each function checks for a specific poker hand. The order in
+    # which they are checked is important, since higher value hands
+    # should be checked first.
+    functions = [is_royal_flush, is_straight_flush, is_four_of_a_kind,
+                 is_full_house, is_flush, is_straight, is_three_of_a_kind,
+                 is_two_pair, is_pair, is_high_card]
+    for i, function in enumerate(functions):
+        result = function(hand)
+        if result:
+            return len(functions) - i, result  # Return ranking and cards
+    return None
+
+def is_royal_flush(hand):
+    if is_flush(hand) and is_straight(hand) and sorted(hand)[4][0] == 'A':
+        return sorted(hand, key=lambda x: RANKS.index(x[0]))
+    return False
+
+def is_straight_flush(hand):
+    if is_flush(hand) and is_straight(hand):
+        return sorted(hand, key=lambda x: RANKS.index(x[0]))
+    return False
+
+def is_four_of_a_kind(hand):
+    ranks = [card[0] for card in hand]
+    for rank in ranks:
+        if ranks.count(rank) == 4:
+            return sorted(hand, key=lambda x: (ranks.count(x[0]), RANKS.index(x[0])), reverse=True)
+    return False
+
+# Similar functions for is_full_house, is_flush, is_straight,
+# is_three_of_a_kind, is_two_pair, is_pair, is_high_card...
+
+def compare_hands(player_hand, dealer_hand):
+    player_value = hand_value(player_hand)
+    dealer_value = hand_value(dealer_hand)
+
+    if player_value > dealer_value:
+        return 'player'
+    elif dealer_value > player_value:
+        return 'dealer'
+    else:
+        return 'tie'
+
+for row in rows:
+    print(row)
